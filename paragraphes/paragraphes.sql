e3f951dc00ee684b2ccc02e420dddb2f6d4b1d98
felix <felixdelcourt@hotmail.fr>
2018-12-24 20:15:38 +0100

    BDD à jour

--- paragraphes/paragraphes.sql
+++ paragraphes/paragraphes.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1
--- http://www.phpmyadmin.net
+-- version 4.6.6deb5
+-- https://www.phpmyadmin.net/
 --
--- Client: localhost
--- Généré le: Dim 03 Janvier 2016 à 23:17
--- Version du serveur: 5.5.46-0ubuntu0.14.04.2
--- Version de PHP: 5.5.9-1ubuntu4.14
+-- Client :  localhost:3306
+-- Généré le :  Lun 24 Décembre 2018 à 20:14
+-- Version du serveur :  5.7.23-0ubuntu0.18.04.1
+-- Version de PHP :  7.2.10-0ubuntu0.18.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,11 +14,54 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Base de données: `paragraphes`
+-- Base de données :  `paragraphes`
 --
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles`
+--
+
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `name` varchar(200) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `articles`
+--
+
+INSERT INTO `articles` (`id`, `name`) VALUES
+(1, 'pantalon'),
+(2, 'chaussette'),
+(25, 'chemise');
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `articles_paragraphes`
+--
+
+CREATE TABLE `articles_paragraphes` (
+  `id_article` int(11) NOT NULL,
+  `id_paragraphe` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Contenu de la table `articles_paragraphes`
+--
+
+INSERT INTO `articles_paragraphes` (`id_article`, `id_paragraphe`) VALUES
+(1, 1),
+(1, 3),
+(2, 2),
+(2, 4),
+(2, 5),
+(25, 80);
 
 -- --------------------------------------------------------
 
@@ -26,13 +69,53 @@ SET time_zone = "+00:00";
 -- Structure de la table `paragraphes`
 --
 
-CREATE TABLE IF NOT EXISTS `paragraphes` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `paragraphes` (
+  `id` int(11) NOT NULL,
   `contenu` text CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `ordre` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+  `ordre` int(11) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=utf8;
 
+--
+-- Contenu de la table `paragraphes`
+--
+
+INSERT INTO `paragraphes` (`id`, `contenu`, `ordre`) VALUES
+(1, 'Hello', 1),
+(3, 'World', 3),
+(4, 'Kahoot', 3),
+(5, 'grosse chaussette', 2),
+(80, 'Belle chemise', 1);
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `paragraphes`
+--
+ALTER TABLE `paragraphes`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=41;
+--
+-- AUTO_INCREMENT pour la table `paragraphes`
+--
+ALTER TABLE `paragraphes`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=81;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
